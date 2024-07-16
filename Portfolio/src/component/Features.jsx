6d7d163f9a10fe642abf7ced9c037d12032b7b2a
SidAsif <asif050md@gmail.com>
2024-07-16 15:13:34 +0530

    Features_Page_added

---
+++ Portfolio/src/component/Features.jsx
@@ -0,0 +1,141 @@
+import { useEffect, useRef, useState } from "react";
+import MoneyImg from "../assets/Money.svg";
+import SecurityImg from "../assets/security.svg";
+import GovernanceImg from "../assets/Governance.svg";
+import AgencyImg from "../assets/Agency.svg";
+import ReliabilityImg from "../assets/Reliability.svg";
+import InteroperabilityImg from "../assets/Interoperability.svg";
+
+const icons = [
+  {
+    img: MoneyImg,
+    title: "Money",
+    description:
+      "Advanced financial analytics for informed decision-making and management.",
+  },
+  {
+    img: SecurityImg,
+    title: "Security",
+    description:
+      "AI-powered solutions ensuring robust data security and privacy.",
+  },
+  {
+    img: GovernanceImg,
+    title: "Governance",
+    description:
+      "Streamlined governance processes with automated tools for efficiency.",
+  },
+  {
+    img: AgencyImg,
+    title: "Agency",
+    description:
+      "Enhanced operational efficiency and client service through AI innovations.",
+  },
+  {
+    img: ReliabilityImg,
+    title: "Reliability",
+    description: "AI-driven quality assurance for dependable operations.",
+  },
+  {
+    img: InteroperabilityImg,
+    title: "Interopability",
+    description:
+      "Seamless integration across systems with AI-enabled solutions.",
+  },
+];
+
+const Features = () => {
+  const [visibleElements, setVisibleElements] = useState([]);
+  const elementsRef = useRef([]);
+
+  useEffect(() => {
+    const currentElements = elementsRef.current;
+
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            setVisibleElements((prev) => [...prev, entry.target]);
+            observer.unobserve(entry.target);
+          }
+        });
+      },
+      { threshold: 0.1 }
+    );
+
+    currentElements.forEach((el) => {
+      if (el) observer.observe(el);
+    });
+
+    return () => {
+      currentElements.forEach((el) => observer.unobserve(el));
+    };
+  }, []);
+
+  return (
+    <>
+      <div className="flex flex-col items-center justify-center min-h-screen bg-black pt-20">
+        <div
+          className={`text-blue-500 font-thin py-2 px-4 rounded-xl mb-4 border border-[#0751BF] border-opacity-35 tracking-[2px] text-[15px] transition-opacity duration-1000 ${
+            visibleElements.includes(elementsRef.current[0])
+              ? "opacity-100"
+              : "opacity-0"
+          }`}
+          ref={(el) => (elementsRef.current[0] = el)}
+        >
+          SOLVING
+        </div>
+        <div className="max-w-5xl md:max-w-7xl lg:max-w-5xl mx-auto text-center mb-14">
+          <h1
+            className={`text-4xl md:text-6xl lg:text-7xl font-medium text-white transition-opacity duration-1000 ${
+              visibleElements.includes(elementsRef.current[1])
+                ? "opacity-100"
+                : "opacity-0"
+            }`}
+            ref={(el) => (elementsRef.current[1] = el)}
+            style={{ lineHeight: "1.1" }}
+          >
+            Powerful new features for solving challenges
+          </h1>
+        </div>
+        <div className="flex items-center justify-center bg-black pb-8">
+          <div className="grid gap-6 p-4 w-full max-w-7xl grid-cols-1 sm:grid-cols-2 sm:grid-rows-3 lg:grid-cols-3 lg:grid-rows-2 ">
+            {icons.map(({ img, title, description }, index) => (
+              <div
+                key={index}
+                ref={(el) => (elementsRef.current[index + 2] = el)}
+                className={`flex flex-col items-center justify-center p-8 bg-black rounded-md shadow-md text-white transition-opacity duration-1000 ${
+                  visibleElements.includes(elementsRef.current[index + 2])
+                    ? "opacity-100"
+                    : "opacity-0"
+                }`}
+                style={{ transitionDelay: `${index * 0.2}s` }}
+              >
+                <div
+                  className="rounded-xl p-6 mb-5 flex items-center justify-center w-20 h-20 "
+                  style={{
+                    background:
+                      "rgb(21,30,42) linear-gradient(174deg, rgba(21,30,42,1) 45%, rgba(0,0,0,1) 100%)",
+                  }}
+                >
+                  <img src={img} alt={title} className="h-20 w-20 mb-2" />
+                </div>
+                <h2 className="text-xl md:text-2xl font-semibold mb-5">
+                  {title}
+                </h2>
+                <p
+                  className="text-center text-[#616d7e] opacity-95"
+                  style={{ lineHeight: 1.4 }}
+                >
+                  {description}
+                </p>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Features;
--- Portfolio/src/component/Home.jsx
+++ Portfolio/src/component/Home.jsx
@@ -18,7 +18,13 @@ const Home = () => {
   //   };
   // }, []);
   return (
-    <section className=" bg-black min-h-screen flex flex-col items-center justify-center pt-36 md:pt-50">
+    <section
+      className=" min-h-screen flex flex-col items-center justify-center pt-36 md:pt-50 "
+      style={{
+        background:
+          "radial-gradient(circle at -12% -44%, rgba(7, 81, 191, 0.6) 0%, rgba(7, 81, 191, 1) 0%, rgba(0, 0, 0, 1) 28%)",
+      }}
+    >
       <div className="max-w-4xl mx-auto px-3 text-center mb-12">
         <h1
           className="text-4xl md:text-5xl lg:text-7xl font-semibold text-white mb-6 "
@@ -27,14 +33,14 @@ const Home = () => {
           Transforming Industries with AI Innovation
         </h1>
         <p
-          className="text-lg md:text-xl text-gray-200 leading-8 mb-8 px-6 md:mx-[80px] text-center opacity-45"
+          className="text-lg md:text-xl text-[#616d7e] leading-8 mb-8 px-6 md:mx-[80px] text-center opacity-90"
           style={{ lineHeight: "1.5" }}
         >
           Welcome to SmartML Labs, where we harness the power of artificial
           intelligence to revolutionize industries and improve everyday
           experiences.
         </p>
-        <button className="bg-blue-500 hover:bg-[#0751BF] text-white text-2xl py-3 px-6 rounded-xl shadow-lg duration-300 transition-transform transform hover:scale-95">
+        <button className="bg-[#2196F3] bg-gradient-to-r from-[#2196F3] to-[#0D3269] text-white text-2xl py-3 px-6 rounded-xl shadow-lg duration-300 transform transition-transform hover:bg-[#0751BF] hover:scale-95">
           Get Started
         </button>
       </div>
